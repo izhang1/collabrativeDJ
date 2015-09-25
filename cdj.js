c12aa448c810bf137eae26231cb3de4c31cc66e2
Spencer Brown <me@spencer.sx>
2015-09-24 23:06:28 -0400

    Remove remaining passport logic

--- cdj.js
+++ cdj.js
@@ -1,33 +1,12 @@
 var express    = require('express');
-var path       = require('path');
 var fs         = require('fs');
 var session    = require('express-session');
 var bodyParser = require('body-parser');
 var http       = require('http');
 var logger     = require('morgan');
-var passport   = require('passport');
-var spotify    = require('passport-spotify');
 
 var config     = null;
 var app        = express();
-
-// Passport serialize the user into the session
-passport.serializeUser(function(user, done) {
-      done(null, user);
-});
-
-// Passport deserialize the user out of the session
-passport.deserializeUser(function(obj, done) {
-      done(null, obj);
-});
-
-// Simple route middleware to ensure user is authenticated.
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.send(401);
-    res.end();
-    return;
-}
 
 fs.readFile("config.json", 'utf8', function(err, data) {
     if (err) {
@@ -56,17 +35,6 @@ fs.readFile("config.json", 'utf8', function(err, data) {
     // all environments
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-    // Set up passport authentication with spotify
-    passport.use(new spotify.Strategy(
-        {
-            clientID: config[configName].spotify.appID,
-            clientSecret: config[configName].spotify.appSecret,
-        },
-        function(accessToken, refreshToken, profile, done) {
-            // find or create DB row for user here
-        }
-    ));
-
     // Set up globals in the req object.
     // Do this first or they will not show up in the req.
     app.use(function(req, res, next) {
@@ -85,9 +53,6 @@ fs.readFile("config.json", 'utf8', function(err, data) {
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: 'false'}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-
 
     // REQUESTS
     app.use(require('./routes.js'));
@@ -96,5 +61,4 @@ fs.readFile("config.json", 'utf8', function(err, data) {
     http.createServer(app).listen(app.get('port'), function(){
         console.log('Express server listening on port ' + app.get('port'));
     });
-
 });
