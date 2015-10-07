940bcd96376d239e99666dbdb38f40842a2bd5f5
Otto45 <austino45@gmail.com>
2015-10-06 20:21:53 -0400

    Added functionality to some of the routes

--- routes.js
+++ routes.js
@@ -10,6 +10,12 @@ module.exports = function (app) {
         var userId = req.body.userId;
         var accessToken = req.body.accessToken;
 
+	// Generate random code to join playlist
+	var timeNow = new Date();
+	var seconds = timeNow.getSeconds();
+	var randomNumber = Math.floor((Math.random() * 1000) + 1);
+	var playlistJoinCode = seconds.toString() + randomNumber.toString();
+
         spotify.createPlaylist(userId, accessToken, function(error, response, body) {
 
             if (error) {
@@ -18,6 +24,10 @@ module.exports = function (app) {
 
             res.send(response.statusCode);
         });
+
+	// TODO: add code to store above playlist join code in database with playlistId created
+	// and the userId
+
     });
 
     app.get('/searchSong', function(req, res) {
@@ -35,10 +45,12 @@ module.exports = function (app) {
     });
 
     app.post('/addSong', function(req, res) {
-        var userId = req.body.userId;
-	var playlistId = req.body.playlistId;
         var accessToken = req.body.accessToken;
 	var songUri = req.body.songUri;
+	var playlistCode = req.body.playlistCode;
+
+	//TODO: Search database to find corresponding playlistId and userId that code matches with.
+	// If neither exist, return 404
 
 	spotify.addSong(userId, playlistId, accessToken, songUri, function(error, response, body) {
 
@@ -52,24 +64,42 @@ module.exports = function (app) {
 
     app.post('/voteSong', function(req, res) {
         var vote = req.body.vote;
+	var playlistCode = req.body.playlistCode;
+
+	//TODO: search database for playlistID corresponding to playlistCode,
+	// then find song in that playlist's table and +1 the vote count for it.
+	// If neither the playlist or song exist, return 404
 
     });
 
     app.post('/deleteSong', function(req, res) {
         var userId = req.body.userId;
-	var playlistId = req.body.playlistId;
+	var playlistCode = req.body.playlistCode;
         var accessToken = req.body.accessToken;
 	var songUri = req.body.songUri;
+
+	//TODO: Search database for playlistId and userId that matches playlistCode, 
+	// return 404 if not found
 
 	spotify.deleteSong(userId, playlistId, accessToken, songUri, function(error, response, body) {
 
 	  if(error) {
 	    // TODO: Handle error
 	  }
+	  else {
+	    // TODO: At this point, we know the user making this request is the playlist owner,
+	    // so delete the song from our database too.
+	  }
 
 	  res.send(response.statusCode);
 	});
 
+    });
+
+    app.post('/joinPlaylist', function(req, res) {
+	var joinCode = req.body.joinCode;
+	
+	//TODO: Search database to see if above code exists, if it doesn't return 404
     });
 
 };
